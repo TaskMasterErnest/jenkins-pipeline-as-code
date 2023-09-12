5e35a921eac3e4256be72b4583333f11e65bccb0
TaskMasterErnest <kluernest08@gmail.com>
2023-09-12 17:57:04 +0000

    updated setup config

--- 01-Packer/setup.sh
+++ 01-Packer/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# yum remove -y java
-sudo yum install -y java-1.8.0-openjdk
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat-stable/jenkins-ci.org.key
-sudo yum install -y jenkins
+sudo dnf update
+sudo dnf install java-11-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo dnf install jenkins -y
 sudo chkconfig jenkins on
 sudo service jenkins start
