e229e68a2cd7573f447b07ba8ec80fe0d97cdfe6
TaskMasterErnest <kluernest08@gmail.com>
2023-09-13 09:22:48 +0000

    added directory for master Jenkins AMI

---
+++ 01-Packer/master/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo ">>>>>>>>>> Installing Amazon Linux extras"
+amazon-linux-extras install epel -y
+
+echo ">>>>>>>>>> Install git"
+sudo dnf install -y git
+
+echo ">>>>>>>>>> Install Jenkins"
+sudo dnf update
+sudo dnf install java-11-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo dnf install jenkins -y
+sudo chkconfig jenkins on
+
+echo ">>>>>>>>>> Setup SSH key"
+mkdir /var/lib/jenkins/.ssh
+touch /var/lib/jenkins/.ssh/known_hosts
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh
+chmod 700 /var/lib/jenkins/.ssh
+mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
+chmod 600 /var/lib/jenkins/.ssh/id_rsa
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
+
+echo ">>>>>>>>>> Configure Jenkins"
+mkdir -p /var/lib/jenkins/init.groovy.d
+mv /tmp/scripts/*.groovy /var/lib/jenkins/init.groovy.d/
+chown -R jenkins:jenkins /var/lib/jenkins/init.groovy.d
+mv /tmp/config/jenkins /etc/sysconfig/jenkins
+chmod +x /tmp/config/install-plugins.sh
+sudo chown -R jenkins:jenkins /var/lib/jenkins/plugins
+bash /tmp/config/install-plugins.sh
+service jenkins start
