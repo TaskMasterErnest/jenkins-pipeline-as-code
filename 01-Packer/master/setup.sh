59cdb0d3695680f04ccb962e6cdc4c65ededb7f4
TaskMasterErnest <kluernest08@gmail.com>
2023-09-27 15:42:59 +0000

    updated configuration

--- 01-Packer/master/setup.sh
+++ 01-Packer/master/setup.sh
@@ -15,35 +15,20 @@ sudo dnf install jenkins -y
 sudo chkconfig jenkins on
 
 echo ">>>>>>>>>> Setup SSH key"
-echo ".............................. 1"
 mkdir -p /var/lib/jenkins/.ssh
-echo ".............................. 2"
 touch /var/lib/jenkins/.ssh/known_hosts
-echo ".............................. 3"
 chown -R jenkins:jenkins /var/lib/jenkins/.ssh
-echo ".............................. 4"
 chmod 700 /var/lib/jenkins/.ssh
-echo ".............................. 5"
 mv /tmp/id_jenkins_ssh /var/lib/jenkins/.ssh/id_jenkins_ssh
-echo ".............................. 6"
 chmod 600 /var/lib/jenkins/.ssh/id_jenkins_ssh
-echo ".............................. 7"
 chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_jenkins_ssh
 
 echo ">>>>>>>>>> Configure Jenkins"
-echo ".............................. 1"
 mkdir -p /var/lib/jenkins/init.groovy.d
-echo ".............................. 2"
 mv /tmp/scripts/*.groovy /var/lib/jenkins/init.groovy.d/
-echo ".............................. 3"
 chown -R jenkins:jenkins /var/lib/jenkins/init.groovy.d
-echo ".............................. 4"
 mv /tmp/config/jenkins /etc/sysconfig/jenkins
-echo ".............................. 5"
 chmod +x /tmp/config/install-plugins.sh
-echo ".............................. 6"
 sudo chown -R jenkins:jenkins /var/lib/jenkins/plugins
-echo ".............................. 7"
 bash /tmp/config/install-plugins.sh
-echo ".............................. 8"
 service jenkins start
